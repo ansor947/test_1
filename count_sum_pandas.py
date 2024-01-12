2e479bbfc51e185554ff40aa709616cd4b909c7d
ansor947 <ansorpy@gmail.com>
2024-01-12 09:06:11 +0300

    first_5

--- count_sum_pandas.py
+++ count_sum_pandas.py
@@ -1,12 +1,20 @@
 import pandas
 
+
+data = {
+    'Дата': ['07.05.2022', '07.05.2022', '08.05.2022', '09.05.2022', '09.05.2022', '09.05.2022'],
+    'Товар': ['Банан', 'Хлеб', 'Банан', 'Хлеб', 'Яблоко', 'Хлеб'],
+    'Количество': [30, 10, 40, 8, 10, 10]
+       }
+
+
 def count_sum(parameters):
     df = pandas.DataFrame(data=parameters)
-    product = df.set_index('product_name')
-    product_index = product.groupby(by=['product_name']).sum()
-    product_index.drop('date', axis= 1 , inplace= True )
+    product = df.set_index('Товар')
+    product_index = product.groupby(by=['Товар']).sum()
+    product_index.drop('Дата', axis= 1 , inplace= True )
     return product_index
 
-# print (count_sum(data))
+print (count_sum(data))
 
 
